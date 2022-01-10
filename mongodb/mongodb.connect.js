bbaa852d87d05f1d22df4f8a6b092ad91f5b8e10
unknown <viniciusalves_96@hotmail.com>
2022-01-10 16:13:38 -0300

    crud completed

--- mongodb/mongodb.connect.js
+++ mongodb/mongodb.connect.js
@@ -1,9 +1,3 @@
-// https://cloud.mongodb.com/v2/
-// Database nodejsStudy
-// Login: vini
-// Senha: 1234
-// mongodb+srv://vini:1234@nodejsstudy.mm6vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
 const mongoose = require("mongoose");
 
 async function connect() {
